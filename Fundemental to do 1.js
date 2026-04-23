8422e9ed812f8996173f097855c122dbb581e86f
Wejdan Hatim <wej.hatim@gmail.com>
2026-04-23 23:36:21 +0300

    function added

--- Fundemental to do 1.js
+++ Fundemental to do 1.js
@@ -1,114 +1,114 @@
-// var myNumber=42;
-// var myName="wejdan";
+var myNumber=42;
+var myName="wejdan";
 
-// var temp=myNumber;
-// myNumber=myName;
-// myName=temp;
+var temp=myNumber;
+myNumber=myName;
+myName=temp;
 
-// console.log(myName);
+console.log(myName);
 
-// //////
+//////
 
-// for (var i=-52 ; i<=1066 ; i++){
-//     console.log(i);
+for (var i=-52 ; i<=1066 ; i++){
+    console.log(i);
 
-// }
+}
 
-///////
-// function beCheerful(){
+/////
+function beCheerful(){
     
-//     for(var i =0 ; i < 98 ; i++){ 
-//         console.log("Good Morning!");
+    for(var i =0 ; i < 98 ; i++){ 
+        console.log("Good Morning!");
 
-// }
-// }
-// beCheerful();
+}
+}
+beCheerful();
 
-// /////////
-// for (var i =-300 ; i<=0 ; i+=3){
-//     if (i === -3 || i=== -6){
-//         continue;
-//     }
-//     console.log(i);
-// }
+/////////
+for (var i =-300 ; i<=0 ; i+=3){
+    if (i === -3 || i=== -6){
+        continue;
+    }
+    console.log(i);
+}
 
-// var i= 2000;
-// while (i <= 5280){
-//     console.log(i);
-//     i++;
-// }
-
-////////////
-// var birthMonth = 7;
-// var birthDay = 30;
-// var month = 7;
-// var day=30;
-// if(birthMonth === month && birthDay ===day || birthMonth === day && birthDay ===month){
-//     console.log("How did you know");
-// } else {
-//     console.log("Just another day");
-// }
+var i= 2000;
+while (i <= 5280){
+    console.log(i);
+    i++;
+}
 
 //////////
-// function isLeapYear (year){
-//     if ((year % 4 ===0 && year % 100 !==0) || (year % 400 ===0)){
-//         console.log("LeapYear")
-//     }
-// else {
-//     console.log("not LeapYear")
-// }
-// }
-// isLeapYear(2000));
+var birthMonth = 7;
+var birthDay = 30;
+var month = 7;
+var day=30;
+if(birthMonth === month && birthDay ===day || birthMonth === day && birthDay ===month){
+    console.log("How did you know");
+} else {
+    console.log("Just another day");
+}
 
-////////////
-// var count =0;
-// for(var i = 512 ; i <= 4096 ; i++){
-//     if(i % 5 ===0){
-//         console.log(i);
-//        count++;
-//     }
+////////
+function isLeapYear (year){
+    if ((year % 4 ===0 && year % 100 !==0) || (year % 400 ===0)){
+        console.log("LeapYear")
+    }
+else {
+    console.log("not LeapYear")
+}
+}
+isLeapYear(2000));
+
+//////////
+var count =0;
+for(var i = 512 ; i <= 4096 ; i++){
+    if(i % 5 ===0){
+        console.log(i);
+       count++;
+    }
      
-// }console.log("total:",count);
-//////////////
-// var i =6;
-// while(i <= 60000){
-//     console.log(i);
-// //     i+=6;
-// // }
-// ///////////////
-// for (var i=1; i<=100 ; i++){
-//     if(i % 10 ===0){
-//         console.log("Dojo");
-//     }
-// else if (i%5 ===0){
-//     console.log("Coding")
-// }
-// else {
-//     console.log(i);
-// }
-// }
-///////////
-// function inputParameter(incoming){
-//     console.log(incoming);
+}console.log("total:",count);
+////////////
+var i =6;
+while(i <= 60000){
+    console.log(i);
+//     i+=6;
 // }
 ///////////////
-// console.log(0); //every negative odd value have positive value
+for (var i=1; i<=100 ; i++){
+    if(i % 10 ===0){
+        console.log("Dojo");
+    }
+else if (i%5 ===0){
+    console.log("Coding")
+}
+else {
+    console.log(i);
+}
+}
+/////////
+function inputParameter(incoming){
+    console.log(incoming);
+}
+/////////////
+console.log(0); //every negative odd value have positive value
 
+/////////
+var i = 2016;
+while (i >0){
+    console.log(i);
+    i-=4
+}
 ///////////
-// var i = 2016;
-// while (i >0){
-//     console.log(i);
-//     i-=4
-// }
-// ///////////
-// var lowNum =2;
-// var highNum =9;
-// var mult=3;
-// for (var i = highNum; i<= lowNum; i--){
-//     if(i % mult ===0 ){
-//         console.log (i);
-//     }
-// }
+var lowNum =2;
+var highNum =9;
+var mult=3;
+for (var i = highNum; i<= lowNum; i--){
+    if(i % mult ===0 ){
+        console.log (i);
+    }
+}
 /////////////
 function countDown(param1 , param2 , param3 , param4){
 var i = param2;
