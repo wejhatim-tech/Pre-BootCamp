d3f1dd87e18da3193db05b443cba5e7044b0a88f
Wejdan Hatim <wej.hatim@gmail.com>
2026-04-23 21:17:29 +0300

    function added

---
+++ main.js
@@ -0,0 +1,18 @@
+var myNumber=42;
+var myName="wejdan";
+
+var temp=myNumber;
+myNumber=myName;
+myName=temp;
+
+console.log(myName);
+
+//////
+
+for (var i=-52 ; i<=1066 ; i++){
+    console.log(i);
+
+}
+
+
+
